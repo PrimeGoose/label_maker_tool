99f95b030d285664af96147b6ee67562d004ba70
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-09-11 17:21:44 +0100

    label height adjust, font weight toggle, stying

--- opacity-slider.js
+++ opacity-slider.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', async (event) => {
 
     const opacitySlider = document.getElementById('opacity-slider');
+    const opacityValueLabel = document.getElementById('opacity-value');
 
     /**
      * Sets the alpha value of the background color of all .bg-yellow elements according to the slider value
      */
     function setBgAlpha() {
         const alphaValue = opacitySlider.value / 100;
+        opacityValueLabel.textContent = opacitySlider.value; // Update the label to show the current value
+
         document.querySelectorAll('.bg-yellow').forEach(element => {
             const bgColor = getComputedStyle(element).backgroundColor;
             const [r, g, b] = bgColor.match(/\d+/g);
@@ -19,10 +22,4 @@ document.addEventListener('DOMContentLoaded', async (event) => {
 
     // Add event listener to the slider
     opacitySlider.addEventListener('input', setBgAlpha);
-
-
-
-
-    
-
-}); // DOMContentLoaded  end here 
+}); // DOMContentLoaded end here 
