488543ed46c80521188edd4b9669c6edc8e3ba16
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-09-30 14:19:24 +0100

    replace bold function to toggle font weigth 100 to 900

--- public/scripts/font_weigth.js
+++ public/scripts/font_weigth.js
@@ -1,30 +1,33 @@
+// File: toggle-font-weight.js
+
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Set up event listener for the font weight toggle switch
     document.getElementById('font-toggle').addEventListener('click', () => {
         const button = document.getElementById('font-toggle');
-        
+
         if (button.classList.contains('not-pressed')) {
-          button.classList.remove('not-pressed');
-          button.classList.add('pressed');
-            updateFontDisplay('Black');
-            toggleFontWeight();
-
+            button.classList.remove('not-pressed');
+            button.classList.add('pressed');
+            updateFontDisplay('900');
+            toggleFontWeight(900);
         } else {
-          button.classList.remove('pressed');
-          button.classList.add('not-pressed');
-            updateFontDisplay('Bold');
-            toggleFontWeight();
-
+            button.classList.remove('pressed');
+            button.classList.add('not-pressed');
+            updateFontDisplay('100');
+            toggleFontWeight(100);
         }
-      });
+    });
+
     /**
-     * Function to update the font display label
+     * Update the font display label based on the font weight
+     * @param {string} fontWeight - The new font weight value
      */
-    function updateFontDisplay(font) {
+    function updateFontDisplay(fontWeight) {
         try {
             const fontDisplay = document.getElementById('font-display');
             if (fontDisplay) {
-                fontDisplay.textContent = font;
+                fontDisplay.textContent = fontWeight;
             } else {
                 console.error('Font display element not found in the document.');
             }
@@ -34,40 +37,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     /**
-     * Function to toggle the font weight
+     * Toggle the font weight by updating the CSS variable
+     * @param {number} fontWeight - The new font weight value
      */
-    function toggleFontWeight() {
+    function toggleFontWeight(fontWeight) {
         try {
-            let currentFont = getComputedStyle(document.documentElement).getPropertyValue('--location-font-family').trim();
-            if (currentFont === "'Inconsolata Bold'") {
-                document.documentElement.style.setProperty('--location-font-family', "'Inconsolata Black'");
-                updateFontDisplay('Black');
-            } else {
-                document.documentElement.style.setProperty('--location-font-family', "'Inconsolata Bold'");
-                updateFontDisplay('Bold');
-            }
+            document.documentElement.style.setProperty('--location-font-weight', fontWeight);
         } catch (error) {
             console.error('An error occurred while toggling the font weight:', error);
         }
     }
-
-    /**
-     * Set up event listener for the font weight toggle switch
-     */
-    function setUpFontToggleListener() {
-        try {
-            const fontToggle = document.getElementById('font-toggle');
-
-            if (fontToggle) {
-                fontToggle.addEventListener('change', toggleFontWeight);
-            } else {
-                console.error('Font toggle switch not found in the document.');
-            }
-        } catch (error) {
-            console.error('An error occurred while setting up the font toggle listener:', error);
-        }
-    }
-
-    // Initialize the setup on DOM content loaded
-    setUpFontToggleListener();
 });
