52ec55325c499ad60fd8939c6b8a21056c9588af
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-10-01 12:33:26 +0100

    clean

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,30 +18,11 @@ module.exports = {
     'bg-opacity-100',
 
   ],
-  theme: {
-    extend: {
-      backgroundOpacity: {
-        '0': '0',
-        '5': '0.05',
-        '10': '0.1',
-        '20': '0.2',
-        '25': '0.25',
-        '30': '0.3',
-        '40': '0.4',
-        '50': '0.5',
-        '60': '0.6',
-        '70': '0.7',
-        '75': '0.75',
-        '80': '0.8',
-        '90': '0.9',
-        '95': '0.95',
-        '100': '1',
-      },
-    },
-  },
-  plugins: [require('@tailwindcss/aspect-ratio')
-    , require('@tailwindcss/forms')
+
+  plugins: [
+    // require('@tailwindcss/aspect-ratio')
+    // , require('@tailwindcss/forms')
     // , require('@tailwindcss/line-clamp')
-    , require('@tailwindcss/typography')
+    // , require('@tailwindcss/typography')
   ],
 };
