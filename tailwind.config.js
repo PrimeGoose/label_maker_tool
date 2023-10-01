cddbde481b9e7b74b26ea4eb45ef3dc02c2f839f
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-10-01 06:02:27 +0100

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,7 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/aspect-ratio')
     , require('@tailwindcss/forms')
-    , require('@tailwindcss/line-clamp')
+    // , require('@tailwindcss/line-clamp')
     , require('@tailwindcss/typography')
   ],
 };
