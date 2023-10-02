7e3b0d3c05a2ce38c72b8a0dee870d7d73524021
Kaspars Bergs <64712362+PrimeGoose@users.noreply.github.com>
2023-10-02 06:51:32 +0100

    fix text cente, on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,9 +20,9 @@ module.exports = {
   ],
 
   plugins: [
-    // require('@tailwindcss/aspect-ratio')
-    // , require('@tailwindcss/forms')
-    // , require('@tailwindcss/line-clamp')
-    // , require('@tailwindcss/typography')
+    require('@tailwindcss/aspect-ratio')
+    , require('@tailwindcss/forms')
+    , require('@tailwindcss/line-clamp')
+    , require('@tailwindcss/typography')
   ],
 };
