3a39e15f0d9f857e20f3967624ba4fc79a9e9272
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-10-20 13:17:01 +0100

    update app.component.html, app.component.ts and styles.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,35 +30,36 @@ interface Label {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
-
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   /**
- * @param {MatDialog} dialog - Material dialog service for opening dialogs.
- */
-  constructor(public dialog: MatDialog) { }
+   * @param {MatDialog} dialog - Material dialog service for opening dialogs.
+   */
+  constructor(public dialog: MatDialog) {}
 
   /** Title of the app */
   title = 'rack-label-tool';
 
   /** Array to hold label objects */
-  Labels: Label[] = [{
-    level1: {
-      digit: "5L4",
-      street: "en",
-      number: "61",
-      side: "b",
-      level: "1"
+  Labels: Label[] = [
+    {
+      level1: {
+        digit: '5L4',
+        street: 'en',
+        number: '61',
+        side: 'b',
+        level: '1',
+      },
+      level2: {
+        digit: '5UQ',
+        street: 'en',
+        number: '61',
+        side: 'b',
+        level: '2',
+      },
     },
-    level2: {
-      digit: "5UQ",
-      street: "en",
-      number: "61",
-      side: "b",
-      level: "2"
-    },
-  }];
+  ];
 
   /**
    * Opens the "How To Use" dialog.
@@ -75,39 +76,39 @@ export class AppComponent {
   locationWidth = '130';
 
   /**
- * Sets the dimensions for  "Five labels per A4 sheet".
- */
+   * Sets the dimensions for  "Five labels per A4 sheet".
+   */
   setFive() {
-    this.labelHeight = `${20 * 1.4}`
-    this.locationFontSize = `${23 * 1.121875}`
-    this.digitFontSize = `${15 * 1.121875}`
+    this.labelHeight = `${20 * 1.4}`;
+    this.locationFontSize = `${23 * 1.121875}`;
+    this.digitFontSize = `${15 * 1.121875}`;
 
-    this.digitWidth = `56`
-    this.locationWidth =`144`
+    this.digitWidth = `56`;
+    this.locationWidth = `144`;
   }
 
   /**
    * Sets the dimensions for the "Six labels per A4 sheet".
    */
   setSix() {
-    this.labelHeight = `${20 * 1.1625}`
-    this.locationFontSize = `${23 * 1.08125}`
-    this.digitFontSize = `${15 * 1.08125}`
+    this.labelHeight = `${20 * 1.1625}`;
+    this.locationFontSize = `${23 * 1.08125}`;
+    this.digitFontSize = `${15 * 1.08125}`;
 
-    this.digitWidth = '54'
-    this.locationWidth='138'
+    this.digitWidth = '54';
+    this.locationWidth = '138';
   }
 
   /**
-  * Sets the dimensions for the "Seven labels per A4 sheet".
-  */
+   * Sets the dimensions for the "Seven labels per A4 sheet".
+   */
   setSeven() {
-    this.labelHeight = "20"
-    this.locationFontSize = '23'
-    this.digitFontSize = '15'
+    this.labelHeight = '20';
+    this.locationFontSize = '23';
+    this.digitFontSize = '15';
 
-    this.digitWidth = '50'
-    this.locationWidth='130'
+    this.digitWidth = '50';
+    this.locationWidth = '130';
   }
 
   /** Opacity related properties */
@@ -115,18 +116,17 @@ export class AppComponent {
   yellowOpacityClass: string = 'bg-opacity-100';
 
   /**
-  * Function to set the background opacity.
-  * @param {any} opacity - The opacity value.
-  */
+   * Function to set the background opacity.
+   * @param {any} opacity - The opacity value.
+   */
   return_bg_opacity_yellowSlider(opacity: any) {
-    this.yellowSlider = opacity
-    this.yellowOpacityClass = `bg-opacity-${this.yellowSlider}`
+    this.yellowSlider = opacity;
+    this.yellowOpacityClass = `bg-opacity-${this.yellowSlider}`;
   }
 
-
   /**
-    * Triggers the print dialog.
-    */
+   * Triggers the print dialog.
+   */
   printLabels(): void {
     window.print();
   }
@@ -139,10 +139,10 @@ export class AppComponent {
   useExcelFile(event: any): void {
     function extractColumn(data: any, columnKey: any) {
       return Object.keys(data)
-        .filter(key => key.startsWith(columnKey))
-        .map(key => data[key].v);
+        .filter((key) => key.startsWith(columnKey))
+        .map((key) => data[key].v);
     }
-    this.Labels = []
+    this.Labels = [];
     let file = event.target.files[0];
     let reader = new FileReader();
     reader.onload = (e) => {
@@ -156,34 +156,38 @@ export class AppComponent {
       console.log(columnValues);
       let formattedData: any = columnValues.map((row: any) => {
         let label = row.trim().toUpperCase();
-        let removeAllsybbols_and_spaces = label.replace(/[^a-zA-Z0-9]/g, "");
+        let removeAllsybbols_and_spaces = label.replace(/[^a-zA-Z0-9]/g, '');
         return removeAllsybbols_and_spaces;
       });
       let temp_labels: any = [];
       formattedData.forEach((label: any) => {
-        let level = label.slice(-1)
-        if (level != 1 && level != 2) { return }
-        if (label.length < 9) { return }
+        let level = label.slice(-1);
+        if (level != 1 && level != 2) {
+          return;
+        }
+        if (label.length < 9) {
+          return;
+        }
         if (level == 1) {
-          let digit = label.slice(0, 3)// needs to be 3 characters
+          let digit = label.slice(0, 3); // needs to be 3 characters
           if (digit.length != 3) {
-            console.log("digit is not 3 characters", digit, "in:", label)
-            return
+            console.log('digit is not 3 characters', digit, 'in:', label);
+            return;
           }
-          let street = label.slice(3, 5)// Alphabetical characters only ,two characters
+          let street = label.slice(3, 5); // Alphabetical characters only ,two characters
           if (street.length != 2) {
-            console.log("street is not 2 characters", street, "in:", label)
-            return
+            console.log('street is not 2 characters', street, 'in:', label);
+            return;
           }
-          let number = label.slice(5, 7)// two digits
+          let number = label.slice(5, 7); // two digits
           if (number.length != 2) {
-            console.log("number is not 2 digits", number, "in:", label)
-            return
+            console.log('number is not 2 digits', number, 'in:', label);
+            return;
           }
-          let side = label.slice(7, 8) // can only be A or B
-          if (side !== "A" && side !== "B") {
-            console.log("l1 side is not A or B", side == 'A', "in:", label)
-            return
+          let side = label.slice(7, 8); // can only be A or B
+          if (side !== 'A' && side !== 'B') {
+            console.log('l1 side is not A or B', side == 'A', 'in:', label);
+            return;
           }
 
           temp_labels.push({
@@ -192,30 +196,30 @@ export class AppComponent {
               street: street,
               number: number,
               side: side,
-              level: level
-            }
-          })
+              level: level,
+            },
+          });
         }
         if (level == 2) {
-          let digit = label.slice(0, 3)// needs to be 3 characters
+          let digit = label.slice(0, 3); // needs to be 3 characters
           if (digit.length != 3) {
-            console.log("digit is not 3 characters", digit, "in:", label)
-            return
+            console.log('digit is not 3 characters', digit, 'in:', label);
+            return;
           }
-          let street = label.slice(3, 5)// Alphabetical characters only ,two characters
+          let street = label.slice(3, 5); // Alphabetical characters only ,two characters
           if (street.length != 2) {
-            console.log("street is not 2 characters", street, "in:", label)
-            return
+            console.log('street is not 2 characters', street, 'in:', label);
+            return;
           }
-          let number = label.slice(5, 7)// two digits
+          let number = label.slice(5, 7); // two digits
           if (number.length != 2) {
-            console.log("number is not 2 digits", number, "in:", label)
-            return
+            console.log('number is not 2 digits', number, 'in:', label);
+            return;
           }
-          let side = label.slice(7, 8) // can only be A or B
-          if (side.toUpperCase() !== "A" && side !== "B") {
-            console.log("side is not A or B", side, "in:", label)
-            return
+          let side = label.slice(7, 8); // can only be A or B
+          if (side.toUpperCase() !== 'A' && side !== 'B') {
+            console.log('side is not A or B', side, 'in:', label);
+            return;
           }
           temp_labels.push({
             level2: {
@@ -223,31 +227,39 @@ export class AppComponent {
               street: street,
               number: number,
               side: side,
-              level: level
-            }
-          })
+              level: level,
+            },
+          });
         }
-      });// end of forEach
+      }); // end of forEach
 
       temp_labels.forEach((label1: any) => {
         if (label1.level1) {
           let match = temp_labels.find((label2: any) => {
-            return label2.level2 &&
+            return (
+              label2.level2 &&
               label1.level1.street === label2.level2.street &&
               label1.level1.number === label2.level2.number &&
-              label1.level1.side === label2.level2.side;
+              label1.level1.side === label2.level2.side
+            );
           });
 
           if (match) {
             this.Labels.push({
               level1: label1.level1,
-              level2: match.level2
+              level2: match.level2,
             });
           }
         }
       });
-
     };
     reader.readAsArrayBuffer(file);
   }
+
+  fontWeightClass: 'font-black' | '' = '';
+  buldToggleLabel='B'
+  toggleBold() {
+    this.fontWeightClass = this.fontWeightClass === 'font-black' ? '' : 'font-black';
+  }
+
 }
